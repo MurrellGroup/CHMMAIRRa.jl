ae4ad5e0e38e9c1641fd08fe87fcaf81ed64e81d
Mark Chernyshev <mark26111@gmail.com>
2025-02-18 17:42:14 +0100

    make Plots.jl into an extra dependency

---
+++ src/plot.jl
@@ -0,0 +1,85 @@
+using DataFrames
+
+function xval2num_blocks(x_vals::Vector{Float64}; max_num_blocks::Int = 50)
+    max_x_value = maximum(x_vals)
+    x_vals_normalized = x_vals ./ max_x_value
+    num_blocks = floor.(Int, x_vals_normalized .* max_num_blocks)
+    return num_blocks
+end
+
+function print_unicode_plot(plot::Vector{Vector{Char}})
+    for row in plot
+        println(join(row))
+    end
+end
+
+function count2nchars(count::Int)
+    return collect(string(" n=", count))
+end
+
+function write_recombfreqplot(plot::Vector{Vector{Char}}, out::String)
+    open(out, "w") do f
+        for row in plot
+            println(f, join(row))
+        end
+    end
+end
+
+function plot_chimerism_per_recombination(chimeras_per_recombination::DataFrame)
+    if isdefined(Base, :get_extension) && Base.get_extension(CHMMAIRRa, :PlotExt) !== nothing
+        return Base.get_extension(CHMMAIRRa, :PlotExt).plotsjl_plot_chimerism_per_recombination(chimeras_per_recombination)
+    else
+        return unicode_plot_chimerism_per_recombination(chimeras_per_recombination)
+    end
+end
+
+function unicode_plot_chimerism_per_recombination(chimeras_per_recombination::DataFrame)
+    xvalues = Vector{Float64}(chimeras_per_recombination.chimeras_normalized)
+    ylabels = Vector{String}(chimeras_per_recombination.recombinations_degapped)
+    counts = Vector{Int}(chimeras_per_recombination.n)
+    return unicode_barplot(xvalues, ylabels, counts)
+end
+
+# create a very simple horizontally-oriented unicode barplot
+function unicode_barplot(xvalues::Vector{Float64}, ylabels::Vector{String}, counts::Vector{Int}; max_num_blocks::Int = 50, x_axis_label::String = "Chimeras normalized", BLOCK = '█')::Vector{Vector{Char}}
+    ## setup numbers we need
+    num_blocks = xval2num_blocks(xvalues)
+    max_label_length = maximum(length.(ylabels))
+    n_labels = count2nchars.(counts)
+    # determine plot width. We need to fit labels, y axis, and bars
+    plot_width = max_label_length + 4 + max_num_blocks + maximum(length.(n_labels))
+    # determine plot height. We need to fit labels, spaces between labels, x axis, and x axis labels
+    plot_height = (length(ylabels) * 2) + 3
+    # determine x axis label start and end
+    x_axis_label_start = max_label_length + 4 + floor(Int, ((max_num_blocks - length(x_axis_label)) / 2))
+    x_axis_label_end = x_axis_label_start + length(x_axis_label) - 1
+    # set up x axis tickmark
+    max_x_value_chars =  collect(string(floor(Int, maximum(xvalues))))
+    x_value_tickmark_start = plot_width - length(max_x_value_chars) + 1
+
+
+    ## setup plot
+
+    plot = [fill(' ', plot_width) for _ in 1:plot_height]
+
+    for i in 1:length(ylabels)
+        # Y axis
+        plot[(i * 2) - 1][max_label_length + 3] = '|'
+        plot[i * 2][max_label_length + 3] = '|'
+        # Y axis labels
+        plot[i * 2][max_label_length - length(ylabels[i]) + 1:max_label_length] .= collect(ylabels[i])
+        # Bars
+        plot[i * 2][max_label_length + 4:max_label_length + 4 + num_blocks[i] - 1] .= BLOCK
+        # counts
+        plot[i * 2][max_label_length + 4 + num_blocks[i]:max_label_length + 4 + num_blocks[i] + length(n_labels[i]) - 1] .= n_labels[i]
+    end
+    plot[end - 2] .= '_'
+    plot[end][x_axis_label_start:x_axis_label_end] .= collect(x_axis_label)
+
+    # print plot
+    print_unicode_plot(plot)
+
+    return plot
+end
+
+
--- src/utils.jl
+++ src/utils.jl
@@ -338,3 +338,47 @@ function convert_column_types!(df::DataFrame, type_map::Dict{String, DataType})
     end
     return df
 end
+
+
+#################### plotting pre-processing ####################
+function get_chimerism_per_recombination(CHMMAIRRa_out::DataFrame; top_n::Int = 10)
+    function recombined_alleles_freqprod(recombs, allele2freq, default_freq)
+        parsed_recombs = parse_recombinations_string(recombs)
+        p = get(allele2freq, parsed_recombs[1].left_allele, default_freq)
+        for el in parsed_recombs
+            p *= get(allele2freq, parsed_recombs[1].right_allele, default_freq)
+        end
+        return p
+    end
+
+    function get_frequencies(df::DataFrame, col::String)
+        cts = combine(groupby(df, col), nrow => :n)
+        cts[!,"frequency"] .= cts[!,"n"] ./ sum(cts[!,"n"])
+        return cts
+    end
+
+    allele_freqs = get_frequencies(CHMMAIRRa_out, "v_call")
+    median_freq = sort(allele_freqs[!, "frequency"])[maximum([floor(Int, nrow(allele_freqs) / 2), 1])]
+    allele2freq = Dict(allele_freqs[!, "v_call"] => allele_freqs[!, "frequency"])
+    chimeras_by_allele = get_frequencies(CHMMAIRRa_out[(.! ismissing.(CHMMAIRRa_out.recombinations_degapped)) .& (CHMMAIRRa_out.recombinations_degapped .!= ""),:], "recombinations_degapped")
+
+    sort!(chimeras_by_allele, :n, rev = true)
+    chimeras_by_allele = chimeras_by_allele[1:minimum([top_n, nrow(chimeras_by_allele)]),:]
+
+
+
+    chimeras_by_allele[!,"recombined_alleles_freqprod"] = [recombined_alleles_freqprod(recombs, allele2freq, median_freq) for recombs in chimeras_by_allele[!, "recombinations_degapped"]]
+    chimeras_by_allele[!,"chimeras_normalized"] .= chimeras_by_allele.n ./ chimeras_by_allele[!,"recombined_alleles_freqprod"]
+    sort!(chimeras_by_allele, :chimeras_normalized, rev = true)
+    return chimeras_by_allele
+end
+
+function parse_recombinations_string(recomb_str)
+    return [parse_recombination_string(recomb) for recomb in split(recomb_str, ";")]
+end
+
+function parse_recombination_string(recomb)
+    recomb = recomb[2:end - 1]
+    recomb_split = split(recomb, ", ")
+    return (left_allele = recomb_split[1], right_allele = recomb_split[2], left_pos_degapped = parse(Int, recomb_split[3]), right_pos_degapped = parse(Int, recomb_split[4]))
+end
